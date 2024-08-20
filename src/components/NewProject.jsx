cb5d05555fcc96a0705e4f2054b8bb6069776803
yueming <bluemoon_cn@hotmail.com>
2024-08-20 20:54:29 +0200

    initial

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,64 @@
+import React,{useRef} from 'react';
+import Input from './Input.jsx';
+import Modal from './Modal.jsx';
+
+export default function NewProject({onAdd, onCancel}) {
+    const modal = useRef();
+
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    
+    function handleSave(){
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const entereddueDate = dueDate.current.value;
+        
+        if(
+            enteredTitle.trim() === '' || 
+            enteredDescription.trim()=== '' || 
+            entereddueDate.trim() === ''
+            ){
+                modal.current.open();
+                return;
+            }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: entereddueDate
+        });
+    }
+
+    return (
+    <>
+    <Modal ref={modal} buttonCaption = "Okay">
+        <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+        <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+    </Modal>    
+    <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+            <li>
+                <button className='text-stone-800 hovedr:text-stone-950' 
+                onClick={onCancel}
+                >
+                    Cancel
+                </button>
+            </li>
+            <li>
+                <button 
+                    className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+                    onClick={handleSave}>
+                    Save</button>
+            </li>
+        </menu>
+        <div>
+            <Input type="text" ref = {title} label= "Title" />
+            <Input ref={description} label ="Description" textarea />
+            <Input type="date"  ref={dueDate} label = "Due date"/>
+        </div>
+    </div>
+    </>
+    )
+}
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import NewTask from './NewTask';
+
+export default function Tasks({tasks,onAdd, onDelete}) {
+    console.log('Tasks received:', tasks);
+    console.log('Deleting task with id:', tasks.id);
+
+    return (
+    <section>
+        <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2> 
+        <NewTask onAdd={onAdd}/>
+        {tasks.length === 0 && (
+            <p className='text-stone-800 mb-4' >
+                This project does not have any tasks yet.
+            </p>
+        )}
+        {tasks.length>0 && 
+            <ul className='p-4 mt-8 rounded-md bg-stone-100'>
+            {tasks.map((task)=>(
+                <li key={task.id} className='flex justify-between my-4'>
+                    <span>{task.text}</span>
+                    <button 
+                    className='text-stone-700 hover:text-red-500'
+                    onClick={()=>onDelete(task.id)}
+                    >
+                    Clear
+                    </button>
+                </li>
+            ))}
+            </ul>}
+        </section>
+    );
+}
